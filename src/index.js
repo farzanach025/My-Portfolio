e1affb0e795ad7201833a4c5796d18d20f799309
farzana <farzanach025@gmail.com>
2023-06-12 20:53:08 -0600

    update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
-import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <HashRouter>
+ReactDOM.render(  
+  <StrictMode>
     <App />
-  </HashRouter>,
+  </StrictMode>,
   document.getElementById("root")
 );
 
